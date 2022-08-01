c8a5f7e1e927b766cf3767b7943fa04a2733f55f
Danila Abramov <67269761+danilaabramov@users.noreply.github.com>
2022-08-02 02:23:18 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import { handleValidationErrors, checkAuth, checkHack } from './utils/index.js'
 
 import { UserController, PostController } from './controllers/index.js'
 
-mongoose.connect("mongodb+srv://danila:1234qwer@cluster0.wiw6q.mongodb.net/mern-blog?retryWrites=true&w=majority")
+mongoose.connect(process.env.MONGODB_URI ? process.env.MONGODB_URI : "mongodb+srv://danila:1234qwer@cluster0.wiw6q.mongodb.net/mern-blog?retryWrites=true&w=majority")
     .then(() => console.log('DB ok'))
     .catch((err) => console.log('DB error', err))
 
@@ -52,7 +52,7 @@ app.delete('/posts/:id', checkAuth, PostController.remove)
 app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, PostController.update)
 app.patch('/posts/:id/comment', checkAuth, handleValidationErrors, PostController.comment)
 
-app.listen(4444, (err) => {
+app.listen(process.env.PORT || 4444, (err) => {
     if (err) return console.log(err)
 
     console.log('Server OK')
