31db188fdd9337b93c73f1c57b40ec83ac1b3a91
Danila Abramov <67269761+danilaabramov@users.noreply.github.com>
2022-08-26 22:57:54 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage})
 
-app.use(express.json())
+app.use(express.json({limit: '1gb'}))
+app.use(express.urlencoded({limit: '1gb', extended: true}));
 app.use(cors())
 app.use('/uploads', express.static('uploads'))
 
@@ -70,6 +71,7 @@ app.listen(process.env.PORT || 4444, (err) => {
 
     console.log('Server OK')
 })
+
 
 
 
